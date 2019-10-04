9e705b39e85923595c09bdccc068d767b0cfdc67
Abdullah Sumsum <abdullahsumsum@Abdullahs-MacBook-Pro.local>
2019-10-04 03:11:12 -0400

    Setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.get("/", (req, res) => {
+  return res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
