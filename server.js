31cee8c4fd02e0b0958e646963f8f8c22d9b2f68
viniciuspxs <90933053+viniciuspxs@users.noreply.github.com>
2023-03-19 22:42:02 +0100

    Add endpoint to return DNS Records

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
-const path = require("path");
 
 app.get("/", (req, res) => {
   return res.send(req.headers);
 });
 
-const PORT = process.env.PORT || 5000;
-
+app.get("/dns", async (_req, res) => {
+  const response = await axios.get("https://api.cloudflare.com/client/v4/zones/9508cc421c336e992591f889278d04a1/dns_records", {
+    headers: {
+      Authorization: "Bearer 7m6YaOpaCOWcRyTgxqUjj-lHDY6ha8dAWvcTpg5V",
+      "Content-Type": "application/json",
+    }
+  });
+  return res.send(response.data);
+});
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
